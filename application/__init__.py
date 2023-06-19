4bad69f1252455e70622f121ebc8209a16edcd6d
Simunition <44754802+Simunition@users.noreply.github.com>
2023-06-19 13:18:26 -0500

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -9,12 +9,12 @@ from model_deployment.ModelClass import WeatherPredictor
 #create flask instance
 app = Flask(__name__)
 
-# UPLOAD_FOLDER = "static/"
-# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "static/"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-# # Check if the upload folder exists, create it if necessary
-# if not os.path.exists(UPLOAD_FOLDER):
-#     os.makedirs(UPLOAD_FOLDER)
+# Check if the upload folder exists, create it if necessary
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 APP_URL = "https://data-detectives.herokuapp.com/"
 
@@ -24,10 +24,10 @@ def predict():
     #get file from request
     data = request.files['gribFile']
     filename = secure_filename(data.filename)
-    #data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     #
     predictor = WeatherPredictor('regr_model.joblib')
-    predictor.make_prediction(data)
+    predictor.make_prediction(os.path.join(UPLOAD_FOLDER, filename))
     csv_out = predictor.output_csv() #outputs CSV with filename as input
     png_out = predictor.plot_geopotential_height() #plots the prediction
 
@@ -49,5 +49,4 @@ def predict():
 
     #code needed to take X_array, format, and send to model for prediction
     #prediction sends output that will be passed back to app for display and potential csv output
-
 
